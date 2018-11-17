e877047c47ab5cb198fbaf033f6cbcacdb5b2094
vikramtambe <vikram.tambe@gmail.com>
2018-11-17 12:41:59 -0600

    UI changes

--- src/front/pages/home/Home.js
+++ src/front/pages/home/Home.js
@@ -115,9 +115,9 @@ class Home extends PureComponent<Props, State> {
                     <H1>Blue Men Quiz</H1>
                   </Header>
                   <div>
-                    <ul style = {{fontSize: 'large'}}>
-                      <li><span style={{fontSize: 'larger'}}>TEXT to <b>(667) 771-2333</b></span> with your name and city. e.g Rahul Dravid,Banglore</li>
-                      <li><span style={{fontSize: 'larger'}}><b>TEXT A or B or C or D</b></span> to answer quiz questions to the same number as mentioned above</li>
+                    <ul style = {{fontSize: 'x-large'}}>
+                      <li><span style={{fontSize: 'x-large'}}>TEXT to <b>(667) 771-2333</b></span> with your name and city. e.g Rahul Dravid,Banglore</li>
+                      <li><span style={{fontSize: 'x-large'}}><b>TEXT A or B or C or D</b></span> to answer quiz questions to the same number as mentioned above</li>
                     </ul>
                   </div>
                 </Col>
